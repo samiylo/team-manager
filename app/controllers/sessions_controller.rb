6d04fa81366fc2f5a4adcf57d420a9a39d1cc4c3
samiylo <skryshu@gmail.com>
2020-06-16 14:06:47 -0500

    update Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,33 @@
 class SessionsController < ApplicationController
 
-    
+    def new
+        if logged_in
+            redirect_to project_path
+        else
+            @user = User.new 
+        end
+
+    end
+
+    def create
+        if auth_hash
+            @user = User.find_or_create_by_omniauth(auth_hash)
+            session[:user_id] = @user.id
+
+            redirect_to project_path
+        else
+            @user = User.find_by(email: params[:user][:email])
+
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id
+                redirect_to project_path
+            else
+                redirect_to new_session_path
+            end
+
+        end
+
+    end
 
 
     private
