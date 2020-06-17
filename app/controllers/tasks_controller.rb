4652e587731553325f2ddfe45ecd5a0e148d7079
samiylo <skryshu@gmail.com>
2020-06-17 13:56:18 -0500

    update controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,14 +4,66 @@ class TasksController < ApplicationController
 
     def index
 
+        if params[:user_id]
+            if User.find(params[:user_id]) == current_user
+                @user = User.find(params[:user_id])
+                @tasks = @user.tasks
+            else
+                redirect_to projects_path
+                flash[:notice] = "Unauthorized Access."
+            end
+        else
+            @tasks = Task.all
+        end
     end
-
+    
+    def new
+        if params[:user_id]
+            @user = User.find(params[:user_id])
+            @task = Task.new(user_id: @user.id)
+    
+        else
+            @user = nil
+            @task = Task.new
+        end
+    
+    
+    end
+    
     def create
-
+    
+        @task = Task.new(task_params)
+    
+        if @task.save
+            redirect_to projects_path
+        else
+            render :new
+        end
     end
-
+    
     def edit
-
+        #@task = Task.find(params[:id])
+    end
+    
+    def update
+    
+        if @task.update(task_params)
+    
+            redirect_to projects_path
+        else
+            render :edit
+        end
+    
+    end
+    
+    def complete
+        @task.update(completed: true)
+        redirect_to projects_path
+    end
+    
+    def destroy
+        @task.destroy
+        redirect_to projects_path
     end
 
     private
