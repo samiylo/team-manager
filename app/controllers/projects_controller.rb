5e4cced1ea767eb53ce389d5ab80364d3076d7f9
samiylo <skryshu@gmail.com>
2020-06-17 13:20:10 -0500

    started Views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,6 +15,39 @@ class ProjectsController < ApplicationController
     end
     
     def create
-        
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+        if @project.update(project_params)
+            redirect_to projects_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @project = Project.find(params[:id])
+        @project.destroy
+        redirect_to projects_path
+    end
+
+    private
+
+    def set_project
+        @project = Project.find(params[:id])
+    end
+
+    def project_params
+        params.require('project').permit(:name)
+    end
+
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
     end
 end
