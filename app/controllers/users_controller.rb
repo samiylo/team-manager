9954ef92710c96774bf7926b4f1ca4e87026ea1f
samiylo <skryshu@gmail.com>
2020-06-17 13:37:11 -0500

    update User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,30 @@
 class UsersController < ApplicationController
     
     def new
+        if logged_in
+            redirect_to projects_path
+            flash[:notice] = "Your are already logged in."
+        else
+            @user = User.new 
+            render layout: 'sessions'
+        end
 
     end
 
     def create
+        @user = User.new(user_params)
+        
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to projects_path
+        else
+            render :new, layout: 'sessions'
+        end
 
+    end
+
+    def most_projects
+        @user = User.most_projects 
     end
 
     private
