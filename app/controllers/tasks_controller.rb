28b0e58f48f1baa667dfe8be080000578224dc9c
samiylo <skryshu@gmail.com>
2020-06-17 13:50:40 -0500

    new Tasks Controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,6 +11,20 @@ class TasksController < ApplicationController
     end
 
     def edit
-        
+
+    end
+
+    private
+
+    def set_task
+        @task = Task.find(params[:id])
+    end
+
+    def task_params
+        params.require(:task).permit(:due_date, :description, :user_id, :project_id)
+    end
+
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
     end
 end
